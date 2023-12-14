feba7cef88452d365fb5d7585bc685fae0bb6982
VamshiKrishna-434 <115861228+VamshiKrishna-434@users.noreply.github.com>
2023-12-14 21:40:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleBtn = document.getElementById("toggleBtn");
+    var detailsContainer = document.getElementById("detailsContainer");
+
+    toggleBtn.addEventListener("click", function () {
+        if (detailsContainer.style.display === "none" || detailsContainer.style.display === "") {
+            detailsContainer.style.display = "block";
+            toggleBtn.innerText = "Hide Details";
+        } else {
+            detailsContainer.style.display = "none";
+            toggleBtn.innerText = "Show Details";
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var emailBtn = document.getElementById("emailBtn");
+
+    emailBtn.addEventListener("click", function () {
+        var emailAddress = 'vamshipotterhead434@gmail.com';
+        var mailtoLink = 'mailto:' + encodeURIComponent(emailAddress);
+        window.location.href = mailtoLink;
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var resumeTab = document.getElementById("resume-tab");
+
+    resumeTab.addEventListener("click", function () {
+        var resumePDF = 'rResume.pdf';
+        window.open(resumePDF, '_blank');
+    });
+});
